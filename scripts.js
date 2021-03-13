e65abb3374aa4baac8e3ebe683b246e60fae7687
Kerry <trankerry94@gmail.com>
2021-03-12 22:12:14 -0800

    added trainer bio carousel

---
+++ scripts.js
@@ -0,0 +1,12 @@
+$(window).scroll(function () {
+    $('nav').toggleClass('scrolled', $(this).scrollTop() > 510);
+  });
+
+  (function($) {
+    "use strict";
+
+    // manual carousel controls
+    $('.next').click(function(){ $('.carousel').carousel('next');return false; });
+    $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
+    
+})(jQuery);
