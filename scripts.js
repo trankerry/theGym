5e090ce5a6c55bff0807d274d9a12f58a240f9f6
Kerry <trankerry94@gmail.com>
2021-03-15 21:30:30 -0700

    worked on carousel

--- scripts.js
+++ scripts.js
@@ -1,10 +1,35 @@
+//for carousel controls
 $(function () {
   $('.next').click(function () { $('.carousel').carousel('next'); return false; });
   $('.prev').click(function () { $('.carousel').carousel('prev'); return false; });
 
-
+//will go from transparent navbar to white
   $(window).scroll(function () {
     $('nav').toggleClass('scrolled', $(this).scrollTop() > 510);
   });
 
+//checking the size of the window
+  $(window).resize(function () {
+    let width = $(window).width();  
+  
+    if (width < 992){
+      $.fn.addingClasses();
+      break;
+    }
+  
+  });
+
+  $.fn.addingClasses = function () {
+    $(".trainer-bio-block").addClass("carousel-item");
+    $(".top-carousel").children(".row").removeClass(".row");
+    $(".top-carousel").removeClass(".carousel-item")
+  }
+
+  // $.fn.removingClasses = function () {
+  //   $(".trainer-bio").removeClass("carousel-item")
+  // }
+
 })
+
+
+
