41bede3e6b13fa5335b51dc555cd1f9b29611917
Kerry <trankerry94@gmail.com>
2022-03-22 13:54:04 -0700

    fixed scroller and link tags

--- scripts.js
+++ scripts.js
@@ -1,47 +1,45 @@
 //for carousel controls
 $(function () {
-  $('.next').click(function () {
-    $('.carousel').carousel('next');
-    return false;
-  });
-  $('.prev').click(function () {
-    $('.carousel').carousel('prev');
-    return false;
-  });
+	$('.next').click(function () {
+		$('.carousel').carousel('next');
+		return false;
+	});
+	$('.prev').click(function () {
+		$('.carousel').carousel('prev');
+		return false;
+	});
 
-  //will go from transparent navbar to white
-  $(window).scroll(function () {
-    $('nav').toggleClass('scrolled', $(this).scrollTop() > 510);
-    $(".navButton").toggleClass('scrolled', $(this).scrollTop() > 510);
-  });
-
-})
+	//will go from transparent navbar to white
+	$(window).scroll(function () {
+		$('nav').toggleClass('scrolled', $(this).scrollTop() > 510);
+		$('.navButton').toggleClass('scrolled', $(this).scrollTop() > 510);
+	});
+});
 
 $(function () {
-  $(".signUpButton").click(function () {
-    $("#signUpModal").modal("show");
-  })
-  $(".loginButton").click(function () {
-    $("#loginModal").modal('show');
-  });
-})
+	$('.signUpButton').click(function () {
+		$('#signUpModal').modal('show');
+	});
+	$('.loginButton').click(function () {
+		$('#loginModal').modal('show');
+	});
+});
 
-// $('#carouselExample').on('slide.bs.carousel', function (e) {
+$('#carouselExample').on('slide.bs.carousel', function (e) {
+	let $e = $(e.relatedTarget);
+	let idx = $e.index();
+	let itemsPerSlide = 4;
+	let totalItems = $('.carousel-item').length;
 
-//   let $e = $(e.relatedTarget);
-//   let idx = e.index();
-//   let itemsPerSlide = 4;
-//   let totalItems = $('.carousel-item').length;
-
-//   if (idx >= totalItems - (itemsPerSlide - 1)) {
-//     let it = itemsPerSlide - (totalItems - idx);
-//     for (var i = 0; i < it; i++) {
-//       // append slides to end
-//       if (e.direction == "left") {
-//         $('.carousel-item').eq(i).appendTo('.carousel-inner');
-//       } else {
-//         $('.carousel-item').eq(0).appendTo('.carousel-inner');
-//       }
-//     }
-//   }
-// });
+	if (idx >= totalItems - (itemsPerSlide - 1)) {
+		let it = itemsPerSlide - (totalItems - idx);
+		for (var i = 0; i < it; i++) {
+			// append slides to end
+			if (e.direction == 'left') {
+				$('.carousel-item').eq(i).appendTo('.carousel-inner');
+			} else {
+				$('.carousel-item').eq(0).appendTo('.carousel-inner');
+			}
+		}
+	}
+});
